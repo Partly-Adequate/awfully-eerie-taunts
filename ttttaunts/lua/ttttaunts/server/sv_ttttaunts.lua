e28fccb53dcb5b829ebedf12432300ae638390e5
Reispfannenfresser <Reispfannenfresser@no-reply.users.github.com>
2020-01-31 08:30:20 +0100

    ttttaunt: removed print messages + checks if files exist + stop function

--- ttttaunts/lua/ttttaunts/server/sv_ttttaunts.lua
+++ ttttaunts/lua/ttttaunts/server/sv_ttttaunts.lua
@@ -3,28 +3,29 @@ local path = "taunt/ttt/"
 
 --get all sound files
 local taunt_files = file.Find("sound/" .. path .. "*.wav", "GAME")
+if taunt_files then
+    for i,taunt_file in ipairs(taunt_files) do
+        --add sound structures for easier access
+        sound.Add({
+            name = taunt_file:sub(1, -5),
+            channel = CHAN_VOICE,
+            volume = 1.0,
+            level = 120,
+            pitch = {100, 100},
+            sound = path .. taunt_file
+        })
 
-for i,taunt_file in ipairs(taunt_files) do
-    print(taunt_file)
-    --add sound structures for easier access
-    sound.Add({
-        name = taunt_file:sub(1, -5),
-        channel = CHAN_VOICE,
-        volume = 1.0,
-        level = 120,
-        pitch = {100, 100},
-        sound = path .. taunt_file
-    })
-
-    --send sounds to clients
-    resource.AddFile("sound/" .. path .. taunt_file)
+        --send sounds to clients
+        resource.AddFile("sound/" .. path .. taunt_file)
+    end
 end
 
 --registers all taunts
 local function RegisterTaunts()
+    -- only register taunts when the gamemode is ttt
     if GAMEMODE_NAME != "terrortown" then return end
-    
-    print("Registering ttttaunts!")
+    if not taunt_files then return end
+
     for _,taunt_file in ipairs(taunt_files) do
         --create taunt structure
         local taunt = {
@@ -37,7 +38,10 @@ local function RegisterTaunts()
         taunt.Taunt = function(ply)
             ply:EmitSound(taunt.name)
         end
-        
+        taunt.StopTaunt = function(ply)
+            ply:StopSound(taunt.name)
+        end
+
         --register taunt
         TAUNT.RegisterTaunt(taunt)
     end
